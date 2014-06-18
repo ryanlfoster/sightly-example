ca4d6b1cfac7b07ccb811adc227051002e4f8fc0
Peeter Piegaze <ppiegaze@adobe.com>
2014-06-18 18:23:45 +0200

    initial

---
+++ bundle/src/main/java/com/adobe/aem/docs/Info.java
@@ -0,0 +1,22 @@
+package com.adobe.aem.docs;
+
+import com.adobe.cq.sightly.WCMUse;
+
+public class Info extends WCMUse {
+    private String title;
+    private String description;
+
+    @Override
+    public void activate() throws Exception {
+        title = getProperties().get("title", "");
+        description = getProperties().get("description", "");
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
